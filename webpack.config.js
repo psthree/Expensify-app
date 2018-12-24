198384735811820cf3f519c9e13db49f01b9de96
ps3 <psthree@aol.com>
2018-12-24 11:58:44 -0500

    updatied web pack and getting errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 //webpack needs at least 2 things
 //1 entry point (app.js inside of SRC dir)
-//2 where to output file webpack builts
+//2 where to output file webpack builds
 
 //we need an absolute path to the output folder
 //run this file though node, node webpack.config.js
@@ -10,31 +10,33 @@
 const path = require('path');
 // console.log(path.join(__dirname, 'public'));
 
-module.exports = {
+module.exports = env => {
+  console.log('env=', env);
+  return {
     entry: './src/app.js',
     output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js'
     },
     module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
+      rules: [
+        {
+          loader: 'babel-loader',
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.s?css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        }
+      ]
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true //always server the index page let react router figure out what to display
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true //always server the index page let react router figure out what to display
     }
+  };
 };
 
 //loader (how webpack transforms these files)
