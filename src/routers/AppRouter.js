67d2fcb4a92dbb621f975f6f1d04b9e4c282df6b
ps3 <psthree@aol.com>
2019-05-29 22:14:53 -0400

    update appRouter.js per Adams suggestion

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Router, Route, Switch, Link, NavLink } from 'react-router-dom';
 //Warning: Please use `require("history").createBrowserHistory` instead of `require("history/createBrowserHistory")`. Support for the latter will be removed in the next major release
-import createHistory from 'history/createBrowserHistory';
+//import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 // import Header from '../components/Header';
 import ExpenseDashboard from '../components/ExpenseDashboard';
 import AddExpense from '../components/AddExpense';
@@ -11,7 +12,8 @@ import NotFound from '../components/NotFound';
 import LoginPage from '../components/loginPage';
 import PrivateRoute from './PrivateRoute';
 
-export const history = createHistory();
+//export const history = createHistory();
+export const history = createBrowserHistory();
 
 const AppRouter = () => (
   <Router history={history}>
